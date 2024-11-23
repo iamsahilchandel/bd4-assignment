15278f6fc6106b9d9acb0386183aacd4a60938de
iamsahilchandel <rajputsahilchandel@gmail.com>
2024-11-23 06:43:54 +0530

    Fixed: vercel sqlite file issue

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 require('dotenv').config();
 
 exports.SERVER_PORT = process.env.SERVER_PORT;
+exports.NODE_ENV = process.env.NODE_ENV;
--- database.js
+++ database.js
@@ -1,9 +1,12 @@
 const sqlite3 = require('sqlite3').verbose();
 const { open } = require('sqlite');
+const { NODE_ENV } = require('./config');
+
+const dbPath = NODE_ENV === 'production' ? '/tmp/database.sqlite' : './database.sqlite';
 
 async function getDatabase() {
   return open({
-    filename: './database.sqlite',
+    filename: dbPath,
     driver: sqlite3.Database,
   });
 }
